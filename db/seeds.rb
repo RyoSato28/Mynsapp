f45eb762e318abf8054a7e484939c8e633aac214
Ryo Sato <ryosato@RyonoMacBook-Air.local>
2021-05-24 02:42:04 +0900

    add users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,25 @@
-User.create!(name:  "Example User",
-  email: "example@railstutorial.org",
-  password:              "foobar",
-  password_confirmation: "foobar",
+User.create!(name:  "Nagi",
+  email: "Nagischool@gmail.com",
+  password:              "kamiumi",
+  password_confirmation: "kamiumi",
   admin: true,
   activated: true,
   activated_at: Time.zone.now)
+
+User.create!(name:  "Miki",
+  email: "Mikinursery@gmail.com",
+  password:              "hoiku0708",
+  password_confirmation: "hoiku0708",
+  activated: true,
+  activated_at: Time.zone.now)
+
+# User.create!(name:  "Example User",
+#   email: "example@railstutorial.org",
+#   password:              "foobar",
+#   password_confirmation: "foobar",
+#   admin: true,
+#   activated: true,
+#   activated_at: Time.zone.now)
 
 # 99.times do |n|
 # name  = Faker::Name.name
